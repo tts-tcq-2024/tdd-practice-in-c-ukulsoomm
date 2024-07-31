91caa85a3385730ea207ab2d0f2c6f77fba6b33c
Umme Kulsoom <107399532+ukulsoomm@users.noreply.github.com>
2024-07-31 14:49:24 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,15 +1,21 @@
 #inlcude <string.h>
 #include <stdlib.h>
 
-int Add_EmptyStringInput(const char* input){
+int EmptyStringInput(const char* input){
   if(input == NULL  || input[0] == '\0' ) { 
     return 0;
   }
   return -1; 
 }
 
-int Add_InputLessThan1000(const char* input){
+int InputLessThan1000(const char* input){
   int value = atoi(input);
   return (value < 1000) ? value : 0;
+}
+
+int Check_InputIsNegative(int value){
+    if (value<0) {
+        throw std::runtime_error("negatives not allowed");
+    }
 }
 
